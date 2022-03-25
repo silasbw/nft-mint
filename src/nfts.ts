f42c2060d94fded1de328f6952c901ee466417ec
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-03-25 14:01:03 -0700

    Move ABIs to separate directory

--- src/nfts.ts
+++ src/nfts.ts
@@ -25,21 +25,21 @@ function defaultMakeMintTransaction(
 export const nfts: NFT[] = [
   {
     name: 'Womxn of Celo',
-    abi: require('./womxn-of-celo-abi.json'),
+    abi: require('./abis/womxn-of-celo.json'),
     contractAddress: '0x50826Faa5b20250250E09067e8dDb1AFa2bdf910',
     mintFee: toWei('2', 'ether'),
     makeMintTransaction: defaultMakeMintTransaction,
   },
   {
     name: 'Celo Doodles',
-    abi: require('./celo-doodles-abi.json'),
+    abi: require('./abis/celo-doodles.json'),
     contractAddress: '0xDAd48E7974878949f1672c92014Cc0615c79746f',
     mintFee: toWei('0', 'ether'),
     makeMintTransaction: defaultMakeMintTransaction,
   },
   {
     name: 'Celostrials',
-    abi: require('./celostrials-abi.json'),
+    abi: require('./abis/celostrials.json'),
     contractAddress: '0xAc80c3c8b122DB4DcC3C351ca93aC7E0927C605d',
     mintFee: toWei('3', 'ether'),
     makeMintTransaction: (

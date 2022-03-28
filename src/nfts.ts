0d490d02f6f444ed67d85e7c5953cea6febf03f0
MuckT <mcgtom10@gmail.com>
2022-03-27 23:10:45 -0700

    Merge branch 'main' into tomm/front-end-0

--- src/nfts.ts
+++ src/nfts.ts
@@ -4,7 +4,7 @@ import { Address } from '@celo/base/lib/address'
 import { AbiItem, toWei } from 'web3-utils'
 import { Contract } from 'web3-eth-contract'
 
-export interface NFT {
+export interface NFTContract {
   name: string
   abi: AbiItem
   contractAddress: Address
@@ -13,6 +13,15 @@ export interface NFT {
     contract: Contract,
     address: Address,
   ): Promise<CeloTxObject<any>>
+  fetchMetadata(contract: Contract, address: Address): Promise<NFTContractMetadata>
+}
+
+export interface NFTContractMetadata {
+  contractAddress: Address
+  name?: string
+  owner?: Address
+  maxSupply?: string
+  totalSupply?: string
 }
 
 function defaultMakeMintTransaction(
@@ -22,13 +31,36 @@ function defaultMakeMintTransaction(
   return contract.methods.mint(1)
 }
 
-export const nfts: NFT[] = [
+async function defaultFetchMetadata(
+  contract: Contract,
+  address: Address,
+): Promise<NFTContractMetadata> {
+  const metadata : NFTContractMetadata = {contractAddress: address}
+
+  metadata.contractAddress = address
+  if (contract.methods.maxSupply) {
+    metadata.maxSupply = await contract.methods.maxSupply().call()
+  }
+  if (contract.methods.totalSupply) {
+    metadata.totalSupply = await contract.methods.totalSupply().call()
+  }
+  if (contract.methods.name) {
+    metadata.name = await contract.methods.name().call()
+  }
+  if (contract.methods.owner) {
+    metadata.owner = await contract.methods.owner().call()
+  }
+  return metadata
+}
+
+export const nfts: NFTContract[] = [
   {
     name: 'Womxn of Celo',
     abi: require('./abis/womxn-of-celo.json'),
     contractAddress: '0x50826Faa5b20250250E09067e8dDb1AFa2bdf910',
     mintFee: toWei('2', 'ether'),
     makeMintTransaction: defaultMakeMintTransaction,
+    fetchMetadata: defaultFetchMetadata,
   },
   {
     name: 'Celo Doodles',
@@ -36,6 +68,7 @@ export const nfts: NFT[] = [
     contractAddress: '0xDAd48E7974878949f1672c92014Cc0615c79746f',
     mintFee: toWei('0', 'ether'),
     makeMintTransaction: defaultMakeMintTransaction,
+    fetchMetadata: defaultFetchMetadata,
   },
   {
     name: 'Celostrials',
@@ -48,5 +81,6 @@ export const nfts: NFT[] = [
     ): Promise<CeloTxObject<any>> => {
       return contract.methods.mint(address, 1)
     },
+    fetchMetadata: defaultFetchMetadata,
   },
 ]

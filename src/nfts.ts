26f5cb10675f0708c496c1d68318880e33eb79d0
Silas Boyd-Wickizer <silasbw@gmail.com>
2022-04-13 06:57:04 -0700

    Add some more NFT projects (#5)

--- src/nfts.ts
+++ src/nfts.ts
@@ -13,7 +13,10 @@ export interface NFTContract {
     contract: Contract,
     address: Address,
   ): Promise<CeloTxObject<any>>
-  fetchMetadata(contract: Contract, address: Address): Promise<NFTContractMetadata>
+  fetchMetadata(
+    contract: Contract,
+    address: Address,
+  ): Promise<NFTContractMetadata>
 }
 
 export interface NFTContractMetadata {
@@ -35,7 +38,7 @@ async function defaultFetchMetadata(
   contract: Contract,
   address: Address,
 ): Promise<NFTContractMetadata> {
-  const metadata : NFTContractMetadata = {contractAddress: address}
+  const metadata: NFTContractMetadata = { contractAddress: address }
 
   metadata.contractAddress = address
   if (contract.methods.maxSupply) {
@@ -66,6 +69,7 @@ export const nfts: NFTContract[] = [
     name: 'Celo Doodles',
     abi: require('./abis/celo-doodles.json'),
     contractAddress: '0xDAd48E7974878949f1672c92014Cc0615c79746f',
+    // TODO(sbw): the mint fee is sliding.
     mintFee: toWei('0', 'ether'),
     makeMintTransaction: defaultMakeMintTransaction,
     fetchMetadata: defaultFetchMetadata,
@@ -81,6 +85,54 @@ export const nfts: NFTContract[] = [
     ): Promise<CeloTxObject<any>> => {
       return contract.methods.mint(address, 1)
     },
+    fetchMetadata: defaultFetchMetadata,
+  },
+  {
+    name: 'ceLoot',
+    abi: require('./abis/celoot.json'),
+    contractAddress: '0x6411549E7201e5587a19b413301BcaA9432aE3c0',
+    mintFee: toWei('1', 'ether'),
+    makeMintTransaction: (
+      contract: Contract,
+      address: Address,
+    ): Promise<CeloTxObject<any>> => {
+      return contract.methods.claim(1)
+    },
+    fetchMetadata: defaultFetchMetadata,
+  },
+  {
+    name: 'CeloEspresso',
+    abi: require('./abis/celo-espresso.json'),
+    contractAddress: '0x7DD354dB71fbFa060070BC0a05d24F87d24A31B7',
+    mintFee: toWei('3', 'ether'),
+    makeMintTransaction: (
+      contract: Contract,
+      address: Address,
+    ): Promise<CeloTxObject<any>> => {
+      return contract.methods.safeMint(address)
+    },
+    fetchMetadata: defaultFetchMetadata,
+  },
+  {
+    name: 'Daopolis',
+    abi: require('./abis/daopolis.json'),
+    contractAddress: '0xc4ea80deCA2415105746639eC16cB0cF8378996A',
+    // TODO(sbw): the mint fee is sliding.
+    mintFee: toWei('10', 'ether'),
+    makeMintTransaction: (
+      contract: Contract,
+      address: Address,
+    ): Promise<CeloTxObject<any>> => {
+      return contract.methods.mint(address, 1)
+    },
+    fetchMetadata: defaultFetchMetadata,
+  },
+  {
+    name: 'ChinChilla Gang',
+    abi: require('./abis/chin-chilla-gang.json'),
+    contractAddress: '0xc8DF51073CD581902b4fb50131d31f29343131F0',
+    mintFee: toWei('3', 'ether'),
+    makeMintTransaction: defaultMakeMintTransaction,
     fetchMetadata: defaultFetchMetadata,
   },
 ]
